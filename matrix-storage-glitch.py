e4b0bd9f7960cb81cb43c36d3c1ef95955fbe9aa
Meox_ <90358001+69b69t@users.noreply.github.com>
2024-07-11 15:19:31 -0700

    why was i doing an HMAC again?

--- matrix-storage-glitch.py
+++ matrix-storage-glitch.py
@@ -6,7 +6,7 @@ import numpy as np
 from time import sleep
 
 from Crypto.Cipher import AES
-from Crypto.Hash import HMAC, SHA256
+from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
 
 homeserver = "matrix.org"
@@ -14,7 +14,7 @@ homeserver = "matrix.org"
 max_upload_endpoint = "https://"+homeserver+"/_matrix/media/v3/config"
 upload_url = "https://"+homeserver+"/_matrix/media/v3/upload"
 download_url = "https://"+homeserver+"/_matrix/media/v3/download/"
-headers = {"Authorization": "ENTER TOKEN HERE. SHOULD LOOK LIKE Bearer fsglkijnrgljnweasdfaewf"}
+headers = {"Authorization": "Bearer rgiklnergl CHANGE THIS. THIS IS YOUR PRIVATE TOKEN"}
 
 #helper functions
 
@@ -51,40 +51,47 @@ def upload_data(data):
     
     while True:
         #post data chunk
-        response = requests.post(upload_url, data=data, headers=headers)
+        
+        try:
+            response = requests.post(upload_url, data=data, headers=headers)
 
-        #if response is 200, good. return content_uri
-        if(response.status_code == 200):
-            content_uri = response.json()["content_uri"]
-            return content_uri
-        #if 429, we were rate limited. wait the necessary time and try again
-        elif(response.status_code == 429):
-            #rate limited. sleep for response ms
-            print("rate limited for " + str(response.json()[retry_after_ms]) + " ms")
-            time.sleep(float(response.json()[retry_after_ms])/1000)
-        #otherwise, error out
-        else:
-            print("ERROR SENDING TO HOMESERVER" + str(response.status_code))
+            #if response is 200, good. return content_uri
+            if(response.status_code == 200):
+                content_uri = response.json()["content_uri"]
+                return content_uri
+            #if 429, we were rate limited. wait the necessary time and try again
+            elif(response.status_code == 429):
+                #rate limited. sleep for response ms
+                print("rate limited for " + str(response.json()[retry_after_ms]) + " ms")
+                time.sleep(float(response.json()[retry_after_ms])/1000)
+            #otherwise, error out
+            else:
+                print("ERROR SENDING TO HOMESERVER" + str(response.status_code))
+        except Exception as e:
+            print("something went wrong during upload. help" + str(e))
 
 def download_data(content_uri):
     slash_index = content_uri.rfind('/')
     characters_after_slash = content_uri[slash_index + 1:]
     
     while True:
-        #get data chunk
-        response = requests.get(download_url + extract_homeserver(content_uri) + "/" + characters_after_slash + '?allow_redirect=true', headers=headers)
-        
-        #if response is 200, good. return content
-        if(response.status_code == 200):
-            return response.content
-        #if 429, we were rate limited. wait the necessary time and try again
-        elif(response.status_code == 429):
-            #rate limited. sleep for response ms
-            print("rate limited for " + str(response.json()[retry_after_ms]) + " ms")
-            time.sleep(float(response.json()[retry_after_ms])/1000)
-        #otherwise, error out
-        else:
-            print("ERROR GETTING FROM HOMESERVER" + str(response.status_code))
+        try:
+            #get data chunk
+            response = requests.get(download_url + extract_homeserver(content_uri) + "/" + characters_after_slash + '?allow_redirect=true', headers=headers)
+            
+            #if response is 200, good. return content
+            if(response.status_code == 200):
+                return response.content
+            #if 429, we were rate limited. wait the necessary time and try again
+            elif(response.status_code == 429):
+                #rate limited. sleep for response ms
+                print("rate limited for " + str(response.json()[retry_after_ms]) + " ms")
+                time.sleep(float(response.json()[retry_after_ms])/1000)
+            #otherwise, error out
+            else:
+                print("ERROR GETTING FROM HOMESERVER" + str(response.status_code))
+        except Exception as e:
+            print("something went wrong during download. help" + str(e))
 
 def encrypt_and_upload(file_path):
     content_blocks = []
@@ -102,27 +109,24 @@ def encrypt_and_upload(file_path):
             
             #get new key, hmac_key and init a AES cipher and HMAC with the new keys
             key = get_random_bytes(32)
-            hmac_key = get_random_bytes(32)
             cipher = AES.new(key, AES.MODE_CTR)
-            hmac_object = HMAC.new(hmac_key, digestmod=SHA256)
 
             #bundle hmac(plaintext, hmac_key) as well
             #calculate ciphertext and hmac
             ciphertext = cipher.encrypt(data)
-            hmac = hmac_object.update(data).digest()
+            sha256 = SHA256.new(data=data).digest()
 
             #write base64 data to content_blocks
             key_b64 = base64.b64encode(key).decode('utf-8')
             nonce_b64 = base64.b64encode(cipher.nonce).decode('utf-8')
-            hmac_key_b64 = base64.b64encode(hmac_key).decode('utf-8')
-            hmac_b64 = base64.b64encode(hmac).decode('utf-8')
+            sha256_b64 = base64.b64encode(sha256).decode('utf-8')
 
             #upload ciphertext and get 
             content_uri = upload_data(ciphertext)
             print(content_uri + " uploaded")
 
             #append the necessary info to the array
-            content_blocks.append([content_uri, key_b64, nonce_b64, hmac_key_b64, hmac_b64])
+            content_blocks.append([content_uri, key_b64, nonce_b64, sha256_b64])
     return content_blocks
 
 def download_and_decrypt(file_path, content_blocks):
@@ -132,12 +136,10 @@ def download_and_decrypt(file_path, content_blocks):
             content_uri = content_block[0]
             key = base64.b64decode(content_block[1])
             nonce = base64.b64decode(content_block[2])
-            hmac_key = base64.b64decode(content_block[3])
-            expected_hmac = base64.b64decode(content_block[4])
+            expected_sha256 = base64.b64decode(content_block[3])
 
             #make cipher and hmac objects
             cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
-            hmac_object = HMAC.new(hmac_key, digestmod=SHA256)
 
             #download ciphertext and decrypt
             ciphertext = download_data(content_uri)
@@ -146,13 +148,17 @@ def download_and_decrypt(file_path, content_blocks):
             #calculate hmac and check against our known hmac
             #on failure print a warning message
             
+            computed_sha256 = SHA256.new(data=data).digest()
+
+            if(expected_sha256 != computed_sha256):
+                print("DATA WAS MODIFIED ON HOMESERVER OR IN TRANSIT")
+
             print(content_uri + " downloaded")
             #print(base64.b64encode(key).decode('utf-8'))
             #print(base64.b64encode(nonce).decode('utf-8'))
             #print(base64.b64encode(hmac_key).decode('utf-8'))
             #print(base64.b64encode(expected_hmac).decode('utf-8'))
 
-            computed_hmac = hmac_object.update(data).digest()
             #print(base64.b64encode(computed_hmac).decode('utf-8'))
 
             #hmac check has passed, append data to file
@@ -163,12 +169,12 @@ def download_and_decrypt(file_path, content_blocks):
 
 
 def main():
-    content_blocks = encrypt_and_upload('/enter/path/to/upload')
+    content_blocks = encrypt_and_upload('/media/meox/69bstorage/rainbowTable/table0Sorted.dat')
 
-    save_to_file('/enter/path/to/save/to', content_blocks)
+    save_to_file("chat_parts", content_blocks)
 
-    content_blocks = read_from_file("/enter/path/to/load/from")
-    download_and_decrypt("/enter/path/to/download/from", content_blocks)
+    #content_blocks = read_from_file("chat_parts")
+    #download_and_decrypt("original_chats.tar", content_blocks)
 
 if __name__ == "__main__":
     main()
